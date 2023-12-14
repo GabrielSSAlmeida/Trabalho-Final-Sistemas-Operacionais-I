70b760bd93b020a382c8bc8e18ca85348f7a8a8f
personal <lhgdantas@gmail.com>
2023-12-13 23:04:10 -0300

    Basicamente funcionando

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,13 @@
 #include <map>
 #include <vector>
 #include <string>
+#include <fstream>
+#include <condition_variable>
+
+
+#define CHAR_BYTES 1
+#define WORD_SIZE 6
+#define HEAD_SIZE 5
 
 
 // ANSI escape codes for text color
@@ -28,17 +35,72 @@ void console_largaTela(){
 
 
 std::mutex mutexContador;
-int contadorInicial = 50;
+int contadorInicial = 500;
 int contador = contadorInicial;
 bool gameOver = false;
 int numTentativasRestantes = 6;
 int acertos = 0;
+bool leituraPronta = false;
 
 std::mutex mutexBuffer;
 std::string buffer("");
 std::mutex mutexTentativas;
 std::mutex mutexAcertos;
 std::mutex gameOverMutex;
+std::mutex mutexArquivo;
+std::condition_variable semaforoArquivo;
+
+typedef std::map<char, std::vector<int>> tabelaLetras;
+
+tabelaLetras letras;
+
+// Função que recebe uma palavra e inicia a dinâmica
+// até que o jogador acerte ou zere suas tentativas
+void ArmazenaPalavra(std::string obj){
+
+    // Cria um map com as letras e respectiva pos na palavra
+    // tabelaLetras letras;
+    letras.clear();
+    for(int i=0; i<static_cast<int>(obj.length()); i++){
+        letras[obj[i]].push_back(i);
+    }
+
+}
+
+
+// Função para ler uma palavra aleatória do arquivo
+void readRandomWord(const std::string& filename, std::string& randomWord){
+    std::ifstream file(filename);
+    int numWords = 0;
+    if(file.is_open()){
+        // Obtém o tamanho total do arquivo
+        file >> numWords;
+
+        // Garante que há pelo menos um caractere no arquivo
+        while(numWords > 0 && !gameOver){
+            srand(static_cast<unsigned>(time(nullptr)));
+            int random = rand() % numWords;
+
+            int byteoffsetWOrd = (random * WORD_SIZE * CHAR_BYTES)+HEAD_SIZE;
+   
+            // Move o ponteiro do arquivo para a posição aleatória
+            file.seekg(byteoffsetWOrd, std::ios::beg);
+
+            // Lê a palavra aleatória
+
+            file >> randomWord;
+            ArmazenaPalavra(randomWord);
+
+
+            std::unique_lock<std::mutex> lock(mutexArquivo);
+            semaforoArquivo.wait(lock, []{ return leituraPronta; });
+
+        }
+        file.close();
+    } else {
+        std::cerr << "Erro ao abrir o arquivo: " << filename << std::endl;
+    }
+}
 
 void PrintLogo(){
     std::string kermo = 
@@ -104,7 +166,7 @@ void PrintTimer(){
 }
 
 void ImprimeGameOver(){
-    std::cout << "Fim de jogo! Sua pontuação foi " << RED << acertos << RESET << "\n\n";
+    std::cout << "Fim de jogo! Sua pontuação foi " << GREEN << acertos << RESET << "\n\n";
     std::cout << "Pressione " << BLUE << "ENTER" << RESET << " para sair.\n";
 }
 
@@ -133,12 +195,6 @@ void imprimeTerminal(){
         }
         std::cout << numTentativasRestantes << "\n\n" << RESET;
 
-        if(numTentativasRestantes == 0){
-            gameOverMutex.lock();
-            gameOver = true;
-            gameOverMutex.unlock();
-        }
-
         mutexTentativas.unlock();
 
         mutexBuffer.lock();
@@ -146,6 +202,16 @@ void imprimeTerminal(){
         mutexBuffer.unlock();
 
         console_largaTela();
+
+        mutexTentativas.lock();
+
+        if(numTentativasRestantes == 0){
+            gameOverMutex.lock();
+            gameOver = true;
+            gameOverMutex.unlock();
+        }
+
+        mutexTentativas.unlock();
     }
 
     ImprimeGameOver();
@@ -154,17 +220,14 @@ void imprimeTerminal(){
 
 
 
-
 std::string verificaPalavra(std::string palavra);
 
+
 void entradaUser(){
-    char aux[2000];
+    char aux[20];
 
-    gameOverMutex.lock();
     while(!gameOver){
-        gameOverMutex.unlock();
-
-        fgets(aux, 2000, stdin);
+        fgets(aux, 20, stdin);
 
         std::string enter(aux);
         enter.pop_back();
@@ -182,45 +245,41 @@ void entradaUser(){
             mutexBuffer.unlock();
         }
 
-        gameOverMutex.lock();
     }
-    gameOverMutex.unlock();
     
 }
 
-
-
-
-typedef std::map<char, std::vector<int>> tabelaLetras;
-
-
-tabelaLetras letras;
 
 // Função que imprime a palavra inserida pelo jogador
 // Cada caracter é impresso de acordo com sua existência em obj
 std::string verificaPalavra(std::string palavra){
 
     std::string retorno = "";
+    bool correctWord = true;
 
     for(int i=0; i<static_cast<int>(palavra.length()); i++){
         char c = palavra[i];
         // O caracter não existe no objetivo
         if(letras[c].empty()){
             retorno += c; // Imprime na cor padrão
+            correctWord = false;
         } else {
             bool posicaoCorreta = false;
+    
             // O caracter existe no objetivo
             for(int pos : letras[c]){
                 if(pos == i){
                     posicaoCorreta = true;
+                }else{
+                    correctWord = false;
                 }
             }
-            if (posicaoCorreta) {
+            if(posicaoCorreta) {
                 // Imprimir o caracter em verde
                 retorno += GREEN;
                 retorno += c;
                 retorno += RESET;
-            } else {
+            }else{
                 // Imprimir o caracter em amarelo
                 retorno += YELLOW;
                 retorno += c; 
@@ -228,34 +287,54 @@ std::string verificaPalavra(std::string palavra){
             }
         }
     }
+
     retorno += "\n";
+
+    
+    if(correctWord){
+        mutexAcertos.lock();
+        acertos++;
+        mutexAcertos.unlock();
+
+        numTentativasRestantes = 6;
+        
+        mutexContador.lock();
+        contador = (contador + 20);
+        if(contador > contadorInicial)
+            contador = contadorInicial;
+        mutexContador.unlock();
+
+        mutexBuffer.lock();
+        buffer = "";
+        mutexBuffer.unlock();
+        
+
+        {
+            std::lock_guard<std::mutex> lock(mutexArquivo);
+            leituraPronta = true;
+        }
+        semaforoArquivo.notify_all();
+    }
 
     return retorno;
 }
 
 
-// Função que recebe uma palavra e inicia a dinâmica
-// até que o jogador acerte ou zere suas tentativas
-void IniciarJogo(std::string obj){
 
-    // Cria um map com as letras e respectiva pos na palavra
-    // tabelaLetras letras;
-    for(int i=0; i<static_cast<int>(obj.length()); i++){
-        letras[obj[i]].push_back(i);
-    }
 
-}
 
-int main() {
-    IniciarJogo("kermo");
-
+int main(){
+    std::string palavra;
+    std::thread p_arquivo(readRandomWord, "palavrasKermo.txt", std::ref(palavra));
+    std::thread p_teclado(entradaUser);
     std::thread p_timer(timer);
     std::thread p_terminal(imprimeTerminal);
-    std::thread p_teclado(entradaUser);
+    
 
     p_teclado.join();
     p_terminal.join();
     p_timer.join();
+    p_arquivo.join();
 
     return 0;
 }
